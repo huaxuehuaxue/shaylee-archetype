53077e3a7fa59d9bd04777d747d1b440abed963e
Adrian <297513931@qq.com>
2020-04-14 21:58:30 +0800

    Gateway，代码结构优化.

--- shaylee-gateway/src/main/java/com/shaylee/gateway/exception/GlobalExceptionHandler.java
+++ shaylee-gateway/src/main/java/com/shaylee/gateway/exception/GlobalExceptionHandler.java
@@ -1,21 +1,16 @@
 package com.shaylee.gateway.exception;
 
-import com.alibaba.fastjson.JSON;
 import com.shaylee.common.core.base.result.Result;
+import com.shaylee.gateway.utils.WebfluxResponseUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.nio.charset.StandardCharsets;
 
 /**
  * Title: 全局异常拦截处理
@@ -34,15 +29,6 @@ public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
     public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
         log.info("GLOBAL EXCEPTION:{}, \n{}", serverWebExchange.getRequest().getPath(), throwable);
         String message = "Sorry, problem occurs. Please contact our staff for help.";
-        return this.response(serverWebExchange, Result.error(message));
-    }
-
-    private Mono<Void> response(ServerWebExchange exchange, Object object) {
-        String json = JSON.toJSONString(object);
-        exchange.getResponse().setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
-        DataBuffer buffer = exchange.getResponse().bufferFactory()
-                .wrap(json.getBytes(StandardCharsets.UTF_8));
-        return exchange.getResponse().writeWith(Flux.just(buffer));
+        return WebfluxResponseUtils.response(serverWebExchange, HttpStatus.INTERNAL_SERVER_ERROR, Result.error(message));
     }
 }
---
+++ shaylee-gateway/src/main/java/com/shaylee/gateway/utils/WebfluxResponseUtils.java
@@ -0,0 +1,29 @@
+package com.shaylee.gateway.utils;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Title: webflux响应工具类
+ * Project: shaylee-common
+ *
+ * @author Adrian
+ * @date 2020-04-14
+ */
+public class WebfluxResponseUtils {
+
+    public static Mono<Void> response(ServerWebExchange exchange, HttpStatus httpStatus, Object object) {
+        String json = JSON.toJSONString(object);
+        exchange.getResponse().setStatusCode(httpStatus);
+        exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(json.getBytes(StandardCharsets.UTF_8));
+        return exchange.getResponse().writeWith(Flux.just(buffer));
+    }
+}
